ba6693b5502965be12aa9556956d7503718e94b4
Your Name <you@example.com>
2020-11-30 16:14:45 +0100

    Adding ehcache dependency and configuring it for spring

--- src/main/java/com/hea/entities/Product.java
+++ src/main/java/com/hea/entities/Product.java
@@ -1,12 +1,20 @@
 package com.hea.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
-public class Product {
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
